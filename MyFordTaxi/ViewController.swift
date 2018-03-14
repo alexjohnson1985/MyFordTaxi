df5ea31910a5806bc261124e84e3f6953f796ced
Alex <alex_johnson85@hotmail.co.uk>
2018-03-14 23:27:28 +0000

    Initial Commit

---
+++ MyFordTaxi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFordTaxi
+//
+//  Created by Alex Johnson on 14/03/2018.
+//  Copyright © 2018 Alex Johnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
